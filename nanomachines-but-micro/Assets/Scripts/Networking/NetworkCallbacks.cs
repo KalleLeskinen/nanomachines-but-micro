b0eb40259ba5c36cfded60273e96dfe7aa40cc39
Kalle Leskinen <kalle@leskinen.org>
2020-09-14 21:56:42 +0300

    Added a mainmenu

---
+++ nanomachines-but-micro/Assets/Scripts/Networking/NetworkCallbacks.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+
+
+[BoltGlobalBehaviour]
+public class NetworkCallbacks : GlobalEventListener
+{
+
+    public override void SceneLoadLocalDone(string scene)
+    {
+        
+        //Random position on table
+        var spawnPos = new Vector3(Random.Range(-2, 2), 1, -4);
+
+        //Insantiate the player vehicle
+        BoltNetwork.Instantiate(BoltPrefabs.temprallycar, spawnPos, Quaternion.identity);
+
+
+    }
+
+
+}
