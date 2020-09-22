8d3bfa4aab3440ad35d6784f375a61795af3e59f
Jonni Uusi-Hakimo <uusihakimo.j@gmail.com>
2020-09-22 11:35:44 +0300

    deleted unnecessary files and moved scripts to /Scripts/ from the project root

---
+++ nanomachines-but-micro/Assets/Scripts/LapTimeUpdate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LapTimeUpdate : MonoBehaviour
+{
+    public Guid id;
+    public List<float> lapTimes;
+    int lap;
+    float clock;
+    // Start is called before the first frame update
+    void Start()
+    {
+        clock = 0;
+        lap = 0;
+        id = Guid.NewGuid(); // generate a guid for the car
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="FinishLine")
+        {
+            if (lap > 0)
+            { 
+                lapTimes.Add(clock);
+            }
+            clock = 0;
+            lap++;
+            Debug.Log("lap:"+ lap);
+        }
+    }
+    private void Update()
+    {
+        clock += Time.deltaTime;
+    }
+}
