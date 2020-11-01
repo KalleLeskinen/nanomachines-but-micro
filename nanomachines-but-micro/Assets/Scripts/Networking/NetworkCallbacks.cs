85d4fb1c299c027a77ab0b1f1b80eae2f22406e2
Jonni Uusi-Hakimo <uusihakimo.j@gmail.com>
2020-11-01 14:40:55 +0200

    car2 torino, truck v1, truck v2 imported

--- nanomachines-but-micro/Assets/Scripts/Networking/NetworkCallbacks.cs
+++ nanomachines-but-micro/Assets/Scripts/Networking/NetworkCallbacks.cs
@@ -14,9 +14,9 @@ public class NetworkCallbacks : GlobalEventListener
         // LocalEvents.Instance.OnCarInstantiate += 
         //Random position on table
         var spawnPos = new Vector3(Random.Range(-2, 2), 1, -4);
-
+        PrefabId[] cars = { BoltPrefabs.Car1_Torino, BoltPrefabs.Car2_Torino, BoltPrefabs.TruckV1, BoltPrefabs.TruckV2 };
         //Insantiate the player vehicle
-        BoltNetwork.Instantiate(BoltPrefabs.Car1_Torino, spawnPos, Quaternion.identity);
+        BoltNetwork.Instantiate(cars[Random.Range(0,cars.Length)], spawnPos, Quaternion.identity);
         //PlayerCamera.Instantiate();
         //Tehkää prefab kamerasta, johon post-processingit yms.
         //if entity.isOwner??
