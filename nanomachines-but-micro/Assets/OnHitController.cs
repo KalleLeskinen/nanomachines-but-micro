2033fff4b05ad4c13169d39fd44cdc4ae206265d
Jonni Uusi-Hakimo <uusihakimo.j@gmail.com>
2020-11-11 12:57:21 +0200

    explosion effect stops the car
    public void Explode()
        {
            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
        }
    }

--- nanomachines-but-micro/Assets/OnHitController.cs
+++ nanomachines-but-micro/Assets/OnHitController.cs
@@ -5,22 +5,8 @@ using UnityEngine;
 
 public class OnHitController : MonoBehaviour
 {
-    public GameObject explosion_effect;
-
-    //void OnCollisionEnter(Collision other)
-    //{
-    //    if (other.gameObject.tag.Equals("rocket") || other.gameObject.tag.Equals("mine"))
-    //    {
-    //        Instantiate(explosion_effect, transform.position, transform.rotation);
-    //    }
-    //    Debug.Log("onhit1");
-
-    //}
-
     public void Explode()
     {
-        Debug.Log("onhit2");
-        GetComponent<Rigidbody>().AddExplosionForce(100000f, gameObject.transform.position, 15f);
-        
+        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
     }
 }
