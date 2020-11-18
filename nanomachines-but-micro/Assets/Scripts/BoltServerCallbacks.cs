f707b28c495ebfe0d6488bdf66b7fee756b14bc9
Jonni Uusi-Hakimo <uusihakimo.j@gmail.com>
2020-11-18 16:17:32 +0200

    commit commit

--- nanomachines-but-micro/Assets/Scripts/BoltServerCallbacks.cs
+++ nanomachines-but-micro/Assets/Scripts/BoltServerCallbacks.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Bolt;
 using Bolt.Utils;
+using System;
 
 [BoltGlobalBehaviour(BoltNetworkModes.Server)]
 public class BoltServerCallbacks : Bolt.GlobalEventListener
@@ -12,13 +13,33 @@ public class BoltServerCallbacks : Bolt.GlobalEventListener
 
     public override void SceneLoadLocalDone(string scene)
     {
+        var spawnEvent = SpawnOnJoin.Create();
+        spawnEvent.id = 0;
+        spawnEvent.Send();
+        
         players = new List<int>();
         players.Add(0);
     }
     public override void SceneLoadRemoteDone(BoltConnection connection)
     {
+        var spawnEvent = SpawnOnJoin.Create();
+        spawnEvent.id = players.Count;
+        spawnEvent.Send();
+        
         players.Add(players.Count);
         Debug.Log("#213" + connection.ToString());
     }
+
+    public override void OnEvent(SpawnOnJoin evnt)
+    {
+        SpawnPlayer(evnt.id);
+    }
+
+    private void SpawnPlayer(int id)
+    {
+        BoltEntity car = BoltNetwork.Instantiate(BoltPrefabs.Truck_1);
+        car.transform.position = new Vector3(id*2, id, id*2);
+    }
+
 
 }
