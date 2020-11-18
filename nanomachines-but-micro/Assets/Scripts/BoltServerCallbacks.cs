3ae5bcbe4777292d6c47f9acec7b4db3c234bf23
Jonni Uusi-Hakimo <uusihakimo.j@gmail.com>
2020-11-18 16:02:10 +0200

    Update BoltServerCallbacks.cs

--- nanomachines-but-micro/Assets/Scripts/BoltServerCallbacks.cs
+++ nanomachines-but-micro/Assets/Scripts/BoltServerCallbacks.cs
@@ -8,12 +8,16 @@ using Bolt.Utils;
 [BoltGlobalBehaviour(BoltNetworkModes.Server)]
 public class BoltServerCallbacks : Bolt.GlobalEventListener
 {
+    public List<int> players;
+
     public override void SceneLoadLocalDone(string scene)
     {
-    
+        players = new List<int>();
+        players.Add(0);
     }
     public override void SceneLoadRemoteDone(BoltConnection connection)
     {
+        players.Add(players.Count);
         Debug.Log("#213" + connection.ToString());
     }
 
