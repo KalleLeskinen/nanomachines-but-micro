165568018df079da4442f5878aeeb480fc857b69
tommitah <48254035+tommitah@users.noreply.github.com>
2020-11-19 14:06:05 +0200

    commit commit

---
+++ nanomachines-but-micro/Assets/Scripts/Networking/PlayerObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObject
+{
+    public BoltEntity entity;
+    public BoltConnection connection;
+
+    public bool IsServer => connection == null;
+
+    public bool IsClient => connection != null;
+}
