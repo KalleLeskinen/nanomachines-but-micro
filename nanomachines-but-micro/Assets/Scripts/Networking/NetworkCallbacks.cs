6eca081e463b0446663cd075c750a6b858c83d4c
Kalle Leskinen <kalle@leskinen.org>
2020-11-24 16:34:02 +0200

    Revert "Merge branch 'core-loop-work-4.1-cam'"
    This reverts commit ea788b5e7ff535d346e0fb246d46d0936acff7ef, reversing
    changes made to 2beb5f7dee85f146ca09c3f4d559075ed6489de0.

--- nanomachines-but-micro/Assets/Scripts/Networking/NetworkCallbacks.cs
+++ nanomachines-but-micro/Assets/Scripts/Networking/NetworkCallbacks.cs
@@ -3,71 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using Bolt;
 using Bolt.Utils;
-using System;
+using UnityEngine.SceneManagement;
+
 
 [BoltGlobalBehaviour]
 public class NetworkCallbacks : GlobalEventListener
 {
-    GameObject raceHandler;
-    int spawnpos;
     public override void SceneLoadLocalDone(string scene)
-    { 
-        //GameObject startpos = GameObject.FindGameObjectWithTag($"{BoltServerIncrementer.GetNextConnectCount()}_pos");
-        GameObject serverPos = GameObject.FindGameObjectWithTag("server_pos");
-
-        //var spawnInTheCorner = new Vector3(5, 3, -15);
-        PrefabId[] cars = { BoltPrefabs.Car1_Torino, BoltPrefabs.Car2_Torino, BoltPrefabs.TruckV1, BoltPrefabs.TruckV2 };
-        //Instantiate the player vehicle
-        //BoltNetwork.Instantiate(cars[Random.Range(0, cars.Length)], spawnInTheCorner, Quaternion.identity);
-
-        //NEW PHYSICS CARS!
-        if (scene == "Level_1")
-        {
-            if (BoltNetwork.IsServer)
-            {
-                BoltEntity serverCar = BoltNetwork.Instantiate(BoltPrefabs.Truck_1, serverPos.transform.position, serverPos.transform.rotation);
-                serverCar.GetComponentInChildren<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-            }
-            else
-            {
-                StartCoroutine(WaitAndSpawn(2));
-            }
-        }
-
-    }
-
-    public override void SceneLoadRemoteDone(BoltConnection connection)
     {
-        foreach (BoltEntity bE in BoltNetwork.Entities)
+        if (SceneManager.GetActiveScene().name == "Level_1")
         {
-            if (bE.StateIs<IVehicleState>())
-            {
-                bE.gameObject.GetComponentInChildren<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-            }
+            //players = GameObject.FindGameObjectsWithTag("Player");
+
+            //Debug.Log($"");
+            //level_1 spawnpos
+            //Vector3 spawnPos = new Vector3(-26.23f, 4.98f, 18.7f);
+
+
+            //var spawnInTheCorner = new Vector3(5, 3, -15);
+            //PrefabId[] cars = { BoltPrefabs.Car1_Torino, BoltPrefabs.Car2_Torino, BoltPrefabs.TruckV1, BoltPrefabs.TruckV2 };
+            //Instantiate the player vehicle
+            //BoltNetwork.Instantiate(cars[Random.Range(0, cars.Length)], spawnInTheCorner, Quaternion.identity);
+
+
+            //NEW PHYSICS CARS!
+            //BoltEntity car = BoltNetwork.Instantiate(BoltPrefabs.Truck_1);
+            //car.transform.position = spawnPos;
+            
         }
     }
 
-    IEnumerator WaitAndSpawn(float time)
-    {
-        yield return new WaitForSeconds(time);
-        raceHandler = GameObject.FindGameObjectWithTag("RaceHandler");
-        spawnpos = raceHandler.GetComponent<BoltEntity>().GetState<IStateOfRace>().NumberOfPlayers;
-        GameObject startpos = GameObject.FindGameObjectWithTag($"{spawnpos}_pos");
-        BoltEntity Car = BoltNetwork.Instantiate(BoltPrefabs.Truck_1, startpos.transform.position, startpos.transform.rotation);
-        Car.GetComponentInChildren<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-    }
 
-
-
-    public override void OnEvent(StartTheGame evnt)
-    {
-        foreach (BoltEntity bE in BoltNetwork.Entities)
-        {
-            if (bE.StateIs<IVehicleState>())
-            {
-                bE.gameObject.GetComponentInChildren<Rigidbody>().constraints = RigidbodyConstraints.None;
-                bE.gameObject.transform.parent = null;
-            }
-        }
-    }
 }
