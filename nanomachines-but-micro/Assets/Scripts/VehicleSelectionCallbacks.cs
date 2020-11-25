47bb8a4c9e45fb9bd89b0bed883d7e7da5061a3b
tommitah <48254035+tommitah@users.noreply.github.com>
2020-11-25 14:36:45 +0200

    VehicleSelectionCallbacks
    commitcompepeg

---
+++ nanomachines-but-micro/Assets/Scripts/VehicleSelectionCallbacks.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Bolt;
+using UnityEngine;
+
+[BoltGlobalBehaviour]
+public class VehicleSelectionCallbacks : GlobalEventListener
+{
+    
+}
