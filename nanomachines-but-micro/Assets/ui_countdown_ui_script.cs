d8197221d3a02bc7963cfe2e56c503f97b7cddd6
Jonni Uusi-Hakimo <uusihakimo.j@gmail.com>
2020-11-28 14:47:36 +0200

    BoltException fixes

--- nanomachines-but-micro/Assets/ui_countdown_ui_script.cs
+++ nanomachines-but-micro/Assets/ui_countdown_ui_script.cs
@@ -15,13 +15,16 @@ public class ui_countdown_ui_script : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() //nää boltentityn ettimiset heittää erroria joskus jostain syystä....: You can't access any Bolt specific methods or properties on an entity which is detached
     {
-        if (Time.frameCount % 10 == 0 && Time.timeSinceLevelLoad > 1)
-            textfield.GetComponent<Text>().text = Mathf.FloorToInt(raceHandler.GetComponent<BoltEntity>().GetState<IStateOfRace>().Clock).ToString();
-        if (Time.timeSinceLevelLoad > 1 && Mathf.FloorToInt(raceHandler.GetComponent<BoltEntity>().GetState<IStateOfRace>().Clock) < 1)
+        if (Time.timeSinceLevelLoad > 1 && raceHandler.GetComponent<BoltEntity>().TryFindState<IStateOfRace>(out IStateOfRace state))
         {
-            textfield.transform.parent.gameObject.SetActive(false);
+            if (Time.frameCount % 10 == 0)
+                textfield.GetComponent<Text>().text = Mathf.FloorToInt(state.Clock).ToString();
+            if (Mathf.FloorToInt(state.Clock) < 1)
+            {
+                textfield.transform.parent.gameObject.SetActive(false);
+            }
         }
     }
 }
