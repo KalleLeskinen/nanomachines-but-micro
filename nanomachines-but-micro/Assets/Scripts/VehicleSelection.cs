9538fdabfb4c56fd4397f4270f64d826383ff064
tommitah <48254035+tommitah@users.noreply.github.com>
2020-11-30 13:53:20 +0200

    VehicleSelection.cs
    Monobehaviour

---
+++ nanomachines-but-micro/Assets/Scripts/VehicleSelection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleSelection : MonoBehaviour
+{
+    private GameObject[] modelPrefabs;
+    
+    private void Awake()
+    {
+        modelPrefabs[0] = Resources.Load("Car1_Torino_Model") as GameObject;
+        modelPrefabs[1] = Resources.Load("Car2_Torino_Model") as GameObject;
+        modelPrefabs[2] = Resources.Load("Truck-1_Model") as GameObject;
+        modelPrefabs[3] = Resources.Load("TruckV1Model") as GameObject;
+    }
+    
+    
+}
