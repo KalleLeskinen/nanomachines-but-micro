cd85fd970c72bc545c9234a853c35b6dbe3a5ef5
tommitah <48254035+tommitah@users.noreply.github.com>
2020-11-30 14:31:32 +0200

    Display
    Käyttäjä voi selata autoja.

--- nanomachines-but-micro/Assets/Scripts/VehicleSelection.cs
+++ nanomachines-but-micro/Assets/Scripts/VehicleSelection.cs
@@ -1,25 +1,74 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class VehicleSelection : MonoBehaviour
 {
-    private GameObject[] modelPrefabs;
-
     [SerializeField]
     private GameObject rotatingDisplay;
 
-    private GameObject displayedModel;
+    private GameObject[] modelPrefabs;
     
+    private GameObject displayedModel;
+
+    private int i;
+
     private void Awake()
     {
+        i = 0;
         displayedModel = new GameObject();
+        modelPrefabs = new GameObject[4];
         modelPrefabs[0] = Resources.Load("Car1_Torino_Model") as GameObject;
         modelPrefabs[1] = Resources.Load("Car2_Torino_Model") as GameObject;
         modelPrefabs[2] = Resources.Load("Truck-1_Model") as GameObject;
         modelPrefabs[3] = Resources.Load("TruckV1Model") as GameObject;
+
+        displayedModel = modelPrefabs[0];
+        DisplayModel(displayedModel);
     }
     
+    private void Update()
+    { 
+        ProcessInput();
+    }
+    
+    private GameObject DisplayModel(GameObject model)
+    {
+        Destroy(displayedModel);
+        displayedModel = new GameObject();
+        return displayedModel = Instantiate(model, rotatingDisplay.transform.position, Quaternion.identity);
+    }
+    
+    private void ProcessInput()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (i == 0)
+            {
+                i = 4;
+                DisplayModel(modelPrefabs[i]);
+            } 
+            else
+            {
+                DisplayModel(modelPrefabs[i--]);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (i == 4)
+            {
+                i = 0;
+                DisplayModel(modelPrefabs[i]);
+            }
+            else
+            {
+                DisplayModel(modelPrefabs[i++]);
+            }
+        }
+    }
     
 }
