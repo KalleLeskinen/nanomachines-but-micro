fc409bf7e55efff472a24d94f5cad669dcf96858
Jonni Uusi-Hakimo <uusihakimo.j@gmail.com>
2020-11-30 20:06:54 +0200

    bugeja fiksattu. .  ..... garagesceneä ei voi ohittaa...... pakko valita
    muuten ei spawnaa mitään

---
+++ nanomachines-but-micro/Assets/spin_the_lift.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin_the_lift : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * 25f * Time.deltaTime);
+    }
+}
