56f9b3f155864d33e87f81738bef605752a1837d
Jonni Uusi-Hakimo <uusihakimo.j@gmail.com>
2020-12-01 13:51:41 +0200

    ammo count and weapons ui
    bottom of the screen

---
+++ nanomachines-but-micro/Assets/weapons_ui_ammo_script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class weapons_ui_ammo_script : MonoBehaviour
+{
+    public GameObject myCar;
+    private void Update()
+    {
+        
+        if (Time.timeSinceLevelLoad > 1 && myCar == null)
+        {
+            foreach (var car in BoltNetwork.Entities)
+            {
+                if (car.TryFindState<IVehicleState>(out IVehicleState state) && car.IsOwner)
+                {
+                    myCar = car;
+                }
+            }
+        }
+        if (Time.frameCount % 30 == 0 && myCar)
+        {
+            UpdateAmmo();
+        }
+    }
+    void UpdateAmmo()
+    {
+        gameObject.GetComponent<Text>().text = myCar.GetComponent<BoltEntity>().GetState<IVehicleState>().AmmoCount.ToString();
+    }
+}
