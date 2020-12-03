8ee65aeb99f1da56e374cd94ce5bca7ef6f1fb9c
vvahla <vahlaville@gmail.com>
2020-12-03 21:07:09 +0200

    menu update

---
+++ nanomachines-but-micro/Assets/Scripts/MenuTextHover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuTextHover : MonoBehaviour
+{
+
+    public GameObject normalText;
+    public GameObject hoverText;
+
+    // Update is called once per frame
+    void OnMouseOver()
+    {
+        Debug.Log("ASDASD");
+        normalText.SetActive(false);
+        hoverText.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        normalText.SetActive(true);
+        hoverText.SetActive(false);
+    }
+}
